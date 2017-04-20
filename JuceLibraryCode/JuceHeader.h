b3ea0206515eb85e95e13d99fce1b96955dc57e0
Nick Thompson <ncthom91@gmail.com>
2017-04-19 21:07:37 -0400

    Update jucer file

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "mxzero";
+    const char* const  projectName    = "Temper";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
